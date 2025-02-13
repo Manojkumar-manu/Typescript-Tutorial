5e1e11b274aa2f538797edeea338854db6a7d898
Manojkumar-manu <manojkumarme022@gmail.com>
2025-02-13 19:13:55 +0530

    ts array  and enum files

---
+++ myArray.ts
@@ -0,0 +1,22 @@
+const superHeros: string[] = [];
+
+// const heroPower :number[]=[]
+
+// one more method to declare this array is
+
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+allUsers.push({name:"",isActive:true})
+
+
+
+
+superHeros.push("hhhh");
+heroPower.push(2);
---
+++ myEnums.ts
@@ -0,0 +1,8 @@
+enum SeatChoice {
+    Aisle ="aisle",
+    Middle=3,
+    Window,
+    Fourth
+}
+
+let hcSeat = SeatChoice.Aisle
